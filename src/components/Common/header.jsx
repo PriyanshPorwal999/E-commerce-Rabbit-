b46e7d2b257433a3a665795c8165db468d6c211b
Priyansh_Porwal <priyanshporwal2004@gmail.com>
2025-06-20 23:44:33 +0530

    Add Navbar in header section

--- src/components/Common/header.jsx
+++ src/components/Common/header.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import TopBar from '../Layout/TopBar'
+import NavBar from './Navbar'
 
-const header = () => {
+const Header = () => {
   return (
     <div>
       <TopBar />
+      <NavBar />
     </div>
   )
 }
 
-export default header
+export default Header
