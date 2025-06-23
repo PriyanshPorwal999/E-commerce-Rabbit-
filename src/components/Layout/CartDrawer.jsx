66033d43f2adb41b8f69137dafdae52e506748ba
Priyansh_Porwal <priyanshporwal2004@gmail.com>
2025-06-23 23:19:32 +0530

    Add checkout button & cart contents

--- src/components/Layout/CartDrawer.jsx
+++ src/components/Layout/CartDrawer.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import { useState } from "react";
 import { IoMdClose } from "react-icons/io";
+import CartContents  from "../Cart/CartContents";
 
-const CartDrawer = ({DrawerOpen, toggleCartDrawer}) => {
-
-
-
+const CartDrawer = ({ DrawerOpen, toggleCartDrawer }) => {
   return (
     <div
-      className={`fixed top-0 right-0 w-3/4 sm:w-1/2 md:w-1/4 h-full bg-white shadow-lg transform transition-transform duration-300 flex flex-col z-50 ${
+      className={`fixed top-0 right-0 w-3/4 sm:w-1/2 md:w-[30rem] h-full bg-white shadow-lg transform transition-transform duration-300 flex flex-col z-50 ${
         DrawerOpen ? "translate-x-0" : "translate-x-full"
       }`}
     >
@@ -17,6 +15,22 @@ const CartDrawer = ({DrawerOpen, toggleCartDrawer}) => {
         <button onClick={toggleCartDrawer}>
           <IoMdClose clasName="h-6 w-6 text-gray-500" />
         </button>
+      </div>
+
+      {/* Cart Contents with scrollable area */}
+      <div className="flex-grow p-4 overflow-y-auto">
+        <h2 className="text-xl font-semibold mb-4">Your Cart</h2>
+        <CartContents />
+      </div>
+
+      {/* Checkout Button */}
+      <div className="p-4 sticky bottom-0 bg-white">
+        <button className="bg-black text-white rounded-lg hover:bg-gray-900 w-full py-3 font-semibold transition">
+          Checkout
+        </button>
+        <p className="text-sm text-gray-400 text-center tracking-tighter mt-2">
+          Shipping, Taxes and Discout codes calculated at checkout.
+        </p>
       </div>
     </div>
   );
