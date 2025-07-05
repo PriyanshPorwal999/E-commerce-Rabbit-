01ac3c302507a12d7409ce17b6ceaac9b1e382f3
Priyansh_Porwal <priyanshporwal2004@gmail.com>
2025-07-05 22:15:31 +0530

    Add route to colletion page

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { Toaster, toast } from "sonner";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Profile from "./pages/Profile";
+import CollectionPage from "./pages/CollectionPage";
 
 
 const App = () => {
@@ -18,6 +19,7 @@ const App = () => {
           <Route path="login" element={<Login />}></Route>
           <Route path="register" element={<Register />}></Route> 
           <Route path="profile" element={<Profile />}></Route> 
+          <Route path="collections/:collection" element={<CollectionPage />}></Route> 
         </Route>
       </Routes>
     </BrowserRouter>
