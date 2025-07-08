dea0c04da7ccf85dfb5bf46a634a53b46c37573c
Priyansh_Porwal <priyanshporwal2004@gmail.com>
2025-07-08 11:59:30 +0530

    Add Route to Products using ProductDetail Component

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Profile from "./pages/Profile";
 import CollectionPage from "./pages/CollectionPage";
+import ProductDetails from "./components/Products/ProductDetails";
 
 
 const App = () => {
@@ -20,6 +21,7 @@ const App = () => {
           <Route path="register" element={<Register />}></Route> 
           <Route path="profile" element={<Profile />}></Route> 
           <Route path="collections/:collection" element={<CollectionPage />}></Route> 
+          <Route path="product/:id" element={<ProductDetails />}></Route> 
         </Route>
       </Routes>
     </BrowserRouter>
