84c1c0a933bc67aec94c89e6ef69fb47ad858533
Priyansh_Porwal <priyanshporwal2004@gmail.com>
2025-07-19 23:47:44 +0530

    Add route to OrderDetailsPage and MyOrdersPage

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,8 @@ import CollectionPage from "./pages/CollectionPage";
 import ProductDetails from "./components/Products/ProductDetails";
 import Checkout from "./components/Cart/Checkout";
 import OrderConfirmationPage from "./pages/OrderConfirmationPage";
-
+import OrderDetailsPage from "./pages/OrderDetailsPage";
+import MyOrdersPage from "./pages/MyOrdersPage";
 
 const App = () => {
   return (
@@ -26,6 +27,8 @@ const App = () => {
           <Route path="product/:id" element={<ProductDetails />}></Route> 
           <Route path="checkout" element={<Checkout />}></Route> 
           <Route path="order-confirmation" element={<OrderConfirmationPage />}></Route> 
+          <Route path="order/:id" element={<OrderDetailsPage />}></Route> 
+          <Route path="my-orders" element={<MyOrdersPage />}></Route> 
         </Route>
       </Routes>
     </BrowserRouter>
