fb6cefb5eec54c9981fcbb024745838099865d3d
Priyansh_Porwal <priyanshporwal2004@gmail.com>
2025-07-21 23:00:39 +0530

    Add Route to admin

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import Checkout from "./components/Cart/Checkout";
 import OrderConfirmationPage from "./pages/OrderConfirmationPage";
 import OrderDetailsPage from "./pages/OrderDetailsPage";
 import MyOrdersPage from "./pages/MyOrdersPage";
+import AdminLayout from "./components/Admin/AdminLayout";
 
 const App = () => {
   return (
@@ -30,6 +31,8 @@ const App = () => {
           <Route path="order/:id" element={<OrderDetailsPage />}></Route> 
           <Route path="my-orders" element={<MyOrdersPage />}></Route> 
         </Route>
+        {/* Admin Layout */}
+        <Route path="/admin" element={<AdminLayout />}></Route>
       </Routes>
     </BrowserRouter>
   );
@@ -37,38 +40,3 @@ const App = () => {
 
 export default App;
 
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
-
-// function App() {
-//   const [count, setCount] = useState(0)
-
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vite.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1 className='text-red-500'>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.jsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   )
-// }
-
-// export default App
