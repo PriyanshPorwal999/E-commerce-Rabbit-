8760e1a3751d5d5a8740b5dab8439c50ae89d267
Priyansh_Porwal <priyanshporwal2004@gmail.com>
2025-08-07 17:08:50 +0530

    Add route to orders page of admin section

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ import AdminHomePage from "./pages/AdminHomePage";
 import UserManagement from "./components/Admin/UserManagement";
 import ProductManagement from "./components/Admin/ProductManagement";
 import EditProductPage from "./components/Admin/EditProductPage";
+import OrderManagement from "./components/Admin/OrderManagement";
 
 const App = () => {
   return (
@@ -43,10 +44,11 @@ const App = () => {
         </Route>
         {/* Admin Layout */}
         <Route path="/admin" element={<AdminLayout />}>
-            <Route index element={<AdminHomePage />}></Route>
-            <Route path="users" element={<UserManagement />}></Route>
-            <Route path="products" element={<ProductManagement />}></Route>
-            <Route path="products/:id/edit" element={<EditProductPage />}></Route>
+          <Route index element={<AdminHomePage />}></Route>
+          <Route path="users" element={<UserManagement />}></Route>
+          <Route path="products" element={<ProductManagement />}></Route>
+          <Route path="products/:id/edit" element={<EditProductPage />}></Route>
+          <Route path="orders" element={<OrderManagement />}></Route>
         </Route>
       </Routes>
     </BrowserRouter>
