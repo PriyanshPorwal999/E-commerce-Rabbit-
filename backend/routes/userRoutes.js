cafd23965e2f3903b21d9fb5c5bbde23158622ad
Priyansh_Porwal <priyanshporwal2004@gmail.com>
2025-08-16 00:28:21 +0530

    Add login route using jwt tokens

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -9,30 +9,99 @@ const router = express.Router();
 // @access Public
 
 router.post("/register", async (req, res) => {
-    const { name, email, password } = req.body;
+  const { name, email, password } = req.body;
 
-    try{
-        // Registration Logic
-        let user = await User.findOne( { email });
+  try {
+    // Registration Logic
+    let user = await User.findOne({ email });
 
-        if (user) return res.status(400).json({message: "user already exists"});
+    if (user) return res.status(400).json({ message: "user already exists" });
 
-        user = new User({name, email, password});
-        await user.save();
+    user = new User({ name, email, password });
+    await user.save();
 
+    // Just for checking that MongoDB is working, written this below commented code
+    // res.status(201).json({
+    //     user: {
+    //         _id: user._id,
+    //         name: user.name,
+    //         email: user.email,
+    //         role: user.role,
+    //     },
+    // })
+
+    // Create JWT Payload
+    const payload = { user: { id: user._id, role: user.role } };
+
+    // Sign and Return the token along with user data
+    jwt.sign(
+      payload,
+      process.env.JWT_SECRET,
+      { expiresIn: "40h" },
+      (err, token) => {
+        if (err) throw err;
+
+        // Send the user and token in response
         res.status(201).json({
-            user: {
-                _id: user._id,
-                name: user.name,
-                email: user.email,
-                role: user.role,
-            },
-        })
-    }
-    catch(error) {
-        console.log(error);
-        res.status(500).send("Server Error");
-    }
+          user: {
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            role: user.role,
+          },
+          token,
+        });
+      }
+    );
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Server Error");
+  }
 });
 
-module.exports = router
+// @route POST /api/users/login
+// @desc Authenticate user
+// @access Public
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    // Find the user by email
+    let user = await User.findOne({ email });
+
+    if (!user) return res.status(400).json({ message: "Invalid Credentials" });
+    const isMatch = await user.matchPassword(password);
+
+    if (!isMatch)
+      return res.status(400).json({ message: "Invalid Credentials" });
+
+    // Create JWT Payload
+    const payload = { user: { id: user._id, role: user.role } };
+
+    // Sign and Return the token along with user data
+    jwt.sign(
+      payload,
+      process.env.JWT_SECRET,
+      { expiresIn: "40h" },
+      (err, token) => {
+        if (err) throw err;
+
+        // Send the user and token in response
+        res.json({
+          user: {
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            role: user.role,
+          },
+          token,
+        });
+      }
+    );
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server Error");
+  }
+});
+
+module.exports = router;
