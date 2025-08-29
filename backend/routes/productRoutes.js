2348ad4d312b8eef6f46cdd4f7620bd056e7b6ea
Priyansh_Porwal <priyanshporwal2004@gmail.com>
2025-08-29 23:25:30 +0530

    updated get route

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -61,12 +61,11 @@ router.post("/", protect, admin, async (req, res) => {
   }
 });
 
-
 // @route PUT /api/:id
 // @desc Update an existing product ID
 // @access Private/Admin
 router.put("/:id", protect, admin, async (req, res) => {
-  try{
+  try {
     const {
       name,
       description,
@@ -92,7 +91,7 @@ router.put("/:id", protect, admin, async (req, res) => {
     // Find product by ID
     const product = await Product.findById(req.params.id);
 
-    if(product){
+    if (product) {
       // Update product fields
       product.name = name || product.name;
       product.description = description || product.description;
@@ -106,25 +105,26 @@ router.put("/:id", protect, admin, async (req, res) => {
       product.collections = collections || product.collections;
       product.material = material || product.material;
       product.gender = gender || product.gender;
-      product.isFeatured = isFeatured !== undefined ? isFeatured : product.isFeatured;
-      product.isPublished = isPublished !== undefined ? isPublished : product.isPublished;
+      product.isFeatured =
+        isFeatured !== undefined ? isFeatured : product.isFeatured;
+      product.isPublished =
+        isPublished !== undefined ? isPublished : product.isPublished;
       product.tags = tags || product.tags;
       product.dimensions = dimensions || product.dimensions;
       product.weight = weight || product.weight;
       product.sku = sku || product.sku;
 
-      // Save the updated product 
+      // Save the updated product
       const updatedProduct = await product.save();
       res.json(updatedProduct);
     } else {
-      res.status(404).json({message: "Product not found"});
+      res.status(404).json({ message: "Product not found" });
     }
-  } catch (error){
+  } catch (error) {
     console.error(error);
     res.status(500).send("Server error");
   }
 });
-
 
 // @route DELETE /api/products/:id
 // @desc Delete a product by ID
@@ -134,14 +134,14 @@ router.delete("/:id", protect, admin, async (req, res) => {
     // Find the product by ID
     const product = await Product.findById(req.params.id);
 
-    if(product){
+    if (product) {
       // Remove the product from DB
       await product.deleteOne();
-      res.json({message: "Product removed"});
+      res.json({ message: "Product removed" });
     } else {
-      res.status(404).json({message: "Product not found"});
+      res.status(404).json({ message: "Product not found" });
     }
-  } catch(error) {
+  } catch (error) {
     console.error(error);
     res.status(500).send("Server Error");
   }
@@ -150,72 +150,86 @@ router.delete("/:id", protect, admin, async (req, res) => {
 // @route GET api/products
 // @desc Get all products with optional query filters
 // @access Public
-router.get("/", async(req, res) => {
-  try{
-    const {collection, size, color, gender, minPrice, maxPrice, sortBy, search, category, material, brand, limit} = req.query;
+router.get("/", async (req, res) => {
+  try {
+    const {
+      collection,
+      size,
+      color,
+      gender,
+      minPrice,
+      maxPrice,
+      sortBy,
+      search,
+      category,
+      material,
+      brand,
+      limit,
+    } = req.query;
     let query = {};
 
     // Filter Logic
-    if(collection && collection.toLocaleLowerCase() != "all"){
+    if (collection && collection.toLocaleLowerCase() != "all") {
       query.collections = collection;
     }
-    if(category && category.toLocaleLowerCase() != "all"){
+    if (category && category.toLocaleLowerCase() != "all") {
       query.category = category;
     }
-    if(material){
-      query.material = {$in: material.split(",")};
+    if (material) {
+      query.material = { $in: material.split(",") };
     }
-    if(brand){
-      query.brand = {$in: brand.split(",")};
+    if (brand) {
+      query.brand = { $in: brand.split(",") };
     }
-    if(size){
-      query.sizes = {$in: size.split(",")};
+    if (size) {
+      query.sizes = { $in: size.split(",") };
     }
-    if(color){
-      query.colors = {$in: [color]};
+    if (color) {
+      query.colors = { $in: [color] };
     }
-    if(gender){
+    if (gender) {
       query.gender = gender;
     }
-    if(minPrice || maxPrice){
+    if (minPrice || maxPrice) {
       query.price = {};
-      if(minPrice) query.price.$gte = Number(minPrice);
-      if(maxPrice) query.price.$lte = Number(maxPrice);
+      if (minPrice) query.price.$gte = Number(minPrice);
+      if (maxPrice) query.price.$lte = Number(maxPrice);
     }
 
-    if(search){
+    if (search) {
       query.$or = [
-        {name: {$regex: search, $options: "i"}},
-        {description: {$regex: search, $options: "i"}},
+        { name: { $regex: search, $options: "i" } },
+        { description: { $regex: search, $options: "i" } },
       ];
     }
 
     // sort logic
     let sort = {};
-    if(sortBy){
-      switch(sortBy){
+    if (sortBy) {
+      switch (sortBy) {
         case "priceAsc":
-          sort = {price: 1};
+          sort = { price: 1 };
           break;
         case "priceDesc":
-          sort = {price: -1};
+          sort = { price: -1 };
+          break;
         case "popularity":
-          sort = {rating : -1};
+          sort = { rating: -1 };
           break;
         default:
-          break;      
+          break;
       }
     }
 
-    // Fetch products and aplly the sorting and limit
+    // Fetch products and apply the sorting and limit
     let products = await Product.find(query)
-        .sort(sort)
-        .limit(Number(limit) || 0);
-    res.json(products);    
-  } catch(error){
+      .sort(sort)
+      .limit(Number(limit) || 0);
+    res.json(products);
+  } catch (error) {
     console.error(error);
     res.status(500).send("Server error");
   }
-})
+});
 
 module.exports = router;
