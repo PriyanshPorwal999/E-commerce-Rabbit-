2e9ac8a49d650bc7a1d0021bbed4ae9692d70b00
Priyansh_Porwal <priyanshporwal2004@gmail.com>
2025-09-04 18:04:14 +0530

    Add cart Item Schema

--- backend/Models/Cart.js
+++ backend/Models/Cart.js
@@ -1,10 +1,11 @@
 const mongoose = require("mongoose");
 
-const cartItemSchema = new mongoose.Schema({
+const cartItemSchema = new mongoose.Schema(
+  {
     productId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Product",
-        required: true,
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+      required: true,
     },
     name: String,
     image: String,
@@ -12,9 +13,30 @@ const cartItemSchema = new mongoose.Schema({
     size: String,
     color: String,
     quantity: {
-        type: Number,
-        default: 1,
+      type: Number,
+      default: 1,
     },
- },
- {_id: false}
+  },
+  { _id: false }
 );
+
+const cartSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    guestId: {
+      type: String,
+    },
+    products: [cartItemSchema],
+    totalPrice: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Cart", cartItemSchema);
